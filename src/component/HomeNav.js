f5ff655527c3a87d88eda404ee6e898032155cd1
Viraj <virajv2000@gmail.com>
2024-07-18 19:51:20 +0700

    solved netlify deployment error

--- src/component/HomeNav.js
+++ src/component/HomeNav.js
@@ -22,7 +22,7 @@ const HomeNav = (props) => {
   };
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
-  <div className="container-fluid">
+  <div className="container-fluid ">
     <Link className="navbar-brand" to="#">Navbar</Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
